33cc2ff929930d69e80f4452c75b6e5ededabee0
verz0 <115163471+verz0@users.noreply.github.com>
2022-12-11 13:17:58 +0530

    Add files via upload

---
+++ Task - 08/index.js
@@ -0,0 +1,34 @@
+ function play1() {
+    var audio = document.getElementById("audio1");
+    audio.play();
+}
+  
+function play2() {
+    var audio = document.getElementById("audio2");
+    audio.play();
+}
+
+function play3() {
+    var audio = document.getElementById("audio3");
+    audio.play();
+}
+
+function play4() {
+    var audio = document.getElementById("audio4");
+    audio.play();
+}
+
+function play5() {
+    var audio = document.getElementById("audio5");
+    audio.play();
+}
+
+function play6() {
+    var audio = document.getElementById("audio6");
+    audio.play();
+}
+
+function play7() {
+    var audio = document.getElementById("audio7");
+    audio.play();
+}  
